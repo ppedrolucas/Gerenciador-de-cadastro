cf4d3a9c88f8f9dcf53504a966f8072183981d11
Hematopoese <pp15lucas@gmail.com>
2024-04-07 11:12:12 -0300

    Layout finalizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        156: "1024px",
+      },
+    },
   },
   plugins: [],
 }
